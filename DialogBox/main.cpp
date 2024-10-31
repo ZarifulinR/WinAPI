f875db2147eb7f62b621adcd5c8efa358f7df8e6
ASUTP <ASUTP@DESKTOP-SSAKK7M>
2024-10-31 16:12:58 +0300

    HwDone

--- DialogBox/main.cpp
+++ DialogBox/main.cpp
@@ -1,6 +1,6 @@
 #include<Windows.h>
 #include"resource.h"
-
+CONST CHAR sz_LOGIN[] = "Ввудите имя пользователя";
 BOOL WINAPI DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lparam);
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmd, INT nCmdShow)
 {
@@ -15,25 +15,47 @@ BOOL WINAPI DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lparam)
 	{
 		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
 		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+		SendMessage(GetDlgItem(hwnd, IDC_EDIT_LOGIN), WM_SETTEXT, 0, (LPARAM)sz_LOGIN);
+
 	}
-		break;
+	break;
 	case WM_COMMAND:
 		switch (LOWORD(wParam))
 		{
-		case IDC_BUTTON_COPY:
+		case IDC_EDIT_LOGIN:
 		{
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE]  {};
+			SendMessage(GetDlgItem(hwnd, IDC_EDIT_LOGIN), WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			if (HIWORD(wParam) == EN_SETFOCUS && strcmp(sz_buffer, sz_LOGIN) == 0)
+				SendMessage(GetDlgItem(hwnd, IDC_EDIT_LOGIN), WM_SETTEXT, 0, (LPARAM)"");
+			if (HIWORD(wParam)== EN_KILLFOCUS&&strcmp(sz_buffer,"")==0)
+				SendMessage(GetDlgItem(hwnd, IDC_EDIT_LOGIN), WM_SETTEXT, 0, (LPARAM)sz_LOGIN);
+		}
+		break;
+		case IDC_BUTTON_COPY:
+
+		{
+			
+			
+
 			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+			/*hEditLogin = GetFocus();
+			if (GetFocus())
+			{
+				SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)"Login");
+			}*/
 			HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
 			//Функция GetDlgItem() возвращает HWND дочернего элемента окна по его ResourceID.
 			// HWND - Handler to window -Это число при помощи которого можно обратится к окну. 
 			CONST INT SIZE = 256;
-			CHAR sz_buffr[SIZE] = {};
-			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffr);
-			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffr);
-
+			CHAR sz_buffer[SIZE] = {};
+			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+		
 		}
 		break;
-		case IDOK: MessageBox(hwnd, "Была нажата кнопка ОК! ", "info", MB_OK | MB_ICONINFORMATION);break;
+		case IDOK: MessageBox(hwnd, "Была нажата кнопка ОК! ", "info", MB_OK | MB_ICONINFORMATION); break;
 		case IDCANCEL:EndDialog(hwnd, 0); break;
 		}
 		break;
