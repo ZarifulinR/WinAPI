76f28b9bab34754f5c3cf728e3bb43fe5f79d000
ASUTP <ASUTP@DESKTOP-SSAKK7M>
2024-11-14 22:08:35 +0300

    DIGITBUTTONSDONE

--- Calc/main.cpp
+++ Calc/main.cpp
@@ -1,120 +1,108 @@
 #include<Windows.h>
-#define IDC_STATIC 1000
-#define IDC_EDIT   1001
-#define IDC_BUTTON 1002
-CONST CHAR g_sz_MY_Window_Class[] = "Calc";
+#include"resource.h"
+const CHAR g_sz_CLASS_NAME[] = "Calc PV_319";
+CONST INT g_i_BUTTON_SIZE = 50;
+CONST INT g_i_INTERVAL = 5;
 
-INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+CONST INT g_i_DISPLAY_WIDTH = 384;
+CONST INT  g_i_DISPLAY_HEIGHT = 22;
 
-INT WINAPI WinMain(HINSTANCE hInmtance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+CONST INT g_i_START_Y = 10;
+CONST INT g_i_START_X = 10;
+CONST INT g_i_DUTTON_START_X = g_i_START_X;
+CONST INT g_i_DUTTON_START_Y = g_i_START_Y + g_i_DISPLAY_HEIGHT + g_i_INTERVAL;
+
+INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM Lparam);
+INT WINAPI WinMain(HINSTANCE hInstanse, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
+	// 1) Регистрация класса окна.
 	WNDCLASSEX wc;
 	ZeroMemory(&wc, sizeof(wc));
-	wc.style = NULL;
+
+	wc.style = 0;
 	wc.cbSize = sizeof(wc);
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
-	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
-	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
-	wc.hInstance = hInmtance;
+	wc.hIcon = LoadIcon(hInstanse, MAKEINTRESOURCE(IDI_ICON1));
+	wc.hIconSm = LoadIcon(hInstanse, MAKEINTRESOURCE(IDI_ICON1));
+	wc.hCursor = LoadCursor(hInstanse, IDC_ARROW);
+	wc.hbrBackground = CreateSolidBrush(RGB(0, 255, 255));
+	wc.hInstance = hInstanse;
+	wc.lpszClassName = g_sz_CLASS_NAME;
 	wc.lpfnWndProc = (WNDPROC)WndProc;
-	wc.lpszClassName = g_sz_MY_Window_Class;
 	wc.lpszMenuName = NULL;
-	if (!RegisterClassEx(&wc) > 0)
+	if (!RegisterClassEx(&wc))
 	{
-		MessageBox(NULL, " Class registrationfaled", "Error", MB_OK | MB_ICONERROR);
+		MessageBox(NULL, "Class registration faled", NULL, MB_OK | MB_ICONERROR);
 	}
-
+	// 2) Создание окна.
 	HWND hwnd = CreateWindowEx
-	(NULL,
-		g_sz_MY_Window_Class,
-		g_sz_MY_Window_Class,
-		WS_OVERLAPPEDWINDOW ,
-		CW_USEDEFAULT, CW_USEDEFAULT ,
-		500, 600,
+	(
+		NULL,
+		g_sz_CLASS_NAME,
+		g_sz_CLASS_NAME,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		CW_USEDEFAULT, CW_USEDEFAULT,
 		NULL,
 		NULL,
-		hInmtance,
+		hInstanse,
 		NULL
+
 	);
-	
 	ShowWindow(hwnd, nCmdShow);
 	UpdateWindow(hwnd);
+	//Запуск окнв.
 	MSG msg;
 	while (GetMessage(&msg, NULL, 0, 0) > 0)
 	{
-		//TranslateMessage(&msg);
-		//DispatchMessage(&msg);
-		IsDialogMessage(hwnd, &msg);
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
 	}
-	return msg.message;
-
+	return msg.wParam;
 	return 0;
 }
-
-INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM Lparam)
 {
 	switch (uMsg)
 	{
 	case WM_CREATE:
-		CreateWindowEx
-		(NULL,
-			"Static",
-			"Это функция создана при помощи функции CreateWindowEx",
-			WS_CHILDWINDOW | WS_VISIBLE ,
-			10,
-			10,
-			500, 25,
-			hwnd,
-			(HMENU)IDC_STATIC,
-			GetModuleHandle(NULL),
-			NULL
+	{
 
-		);
-		CreateWindowEx
-		(NULL,
-			"Edit",
-			"",
-			WS_CHILDWINDOW | WS_VISIBLE | WS_BORDER ,
-			10,
-			48,
-			450,
-			23,
-			hwnd,
-			(HMENU)IDC_EDIT,
-			GetModuleHandle(NULL),
-			NULL
-		);
-		CreateWindowEx
+		HWND hEdit = CreateWindowEx
 		(
-			NULL,
-			"Button",
-			"Применить",
-			WS_CHILD | WS_VISIBLE | WS_TABSTOP,
-			310, 88,
-			150, 32,
+			NULL, "Edit", "",
+			WS_CHILD | WS_VISIBLE | WS_BORDER,
+			15, 15,
+			384, 30,
 			hwnd,
-			(HMENU)IDC_BUTTON,
+			(HMENU)IDI_EDIT_DISPLAY,
 			GetModuleHandle(NULL),
 			NULL
-
 		);
-		break;
-	case WM_COMMAND:
-		switch (LOWORD(wParam))
+		CHAR sz_digit[2] = "0";
+		for (int i = 6; i >= 0; i -= 3)
 		{
-		case IDC_BUTTON:
-			CONST INT SIZE = 256;
-			CHAR sz_buffer[SIZE]{};
-			HWND hStatic = GetDlgItem(hwnd, IDC_STATIC);
-			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
-			SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
-			SendMessage(hStatic,WM_SETTEXT,0,(LPARAM)sz_buffer);
-			SendMessage(hwnd,WM_SETTEXT,0,(LPARAM)sz_buffer);
-			SendMessage(GetDlgItem(hwnd,IDC_BUTTON),WM_SETTEXT,0,(LPARAM)sz_buffer);
-			break;
+			for (int j = 0; j < 3; j++)
+			{
+				sz_digit[0] = 49 + i + j;
+				CreateWindowEx
+				(
+					NULL, "Button", sz_digit,
+					WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+					g_i_DUTTON_START_X * (24) + j * (g_i_BUTTON_SIZE + g_i_INTERVAL), 
+					g_i_DUTTON_START_Y *(2)+ (g_i_BUTTON_SIZE + g_i_INTERVAL) * (2 - i / 3),
+					g_i_BUTTON_SIZE, g_i_BUTTON_SIZE,
+					hwnd,
+					(HMENU)IDC_BUTTON_0 + i + j,
+					GetModuleHandle(NULL),
+					NULL
+				);
+			}
 		}
+	}
+	break;
+	case WM_COMMAND:
 		break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
@@ -122,8 +110,6 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_CLOSE:
 		DestroyWindow(hwnd);
 		break;
-	default:return DefWindowProc(hwnd, uMsg, wParam, lParam);
-		break;
+	default: return DefWindowProc(hwnd, uMsg, wParam, Lparam);
 	}
-	return FALSE;
 }
